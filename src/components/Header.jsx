53e7129f1210120c0114baa5d1c15d6f619a5c04
Ha Nguyen <31268683+earlybird-dev@users.noreply.github.com>
2022-12-06 13:10:55 +1100

    update social link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,13 +7,28 @@ const Header = () => {
         <div className="navbar d-flex flex-column-reverse flex-sm-row justify-content-between mt-3 mb-5">
           <div className="navbar-name">early-bird.dev</div>
           <div className="navbar-social d-flex gap-4 pb-1 pb-md-3">
-            <a href="#contact" className="p-2">
+            <a
+              href="https://github.com/earlybird-dev"
+              rel="noreferrer"
+              target="_blank"
+              className="p-2"
+            >
               <i className="fa fa-github icon-3d"></i>
             </a>
-            <a href="#contact" className="p-2">
+            <a
+              href="https://au.linkedin.com/in/trungha-nguyen"
+              rel="noreferrer"
+              target="_blank"
+              className="p-2"
+            >
               <i className="fa fa-linkedin icon-3d"></i>
             </a>
-            <a href="#contact" className="p-2">
+            <a
+              href="mailto:trungha.dev@gmail.com"
+              rel="noreferrer"
+              target="_blank"
+              className="p-2"
+            >
               <i className="fa fa-envelope icon-3d"></i>
             </a>
           </div>
