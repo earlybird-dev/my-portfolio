a531fd8c7eb0f1d4630efc65a32652c5d22c79db
Ha Nguyen <31268683+earlybird-dev@users.noreply.github.com>
2022-12-10 17:11:04 +1100

    Adjust clouds position and speed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,22 @@ const Header = () => {
   return (
     <>
       <header className="header min-vh-100 pb-5">
+        <div className="container-fluid m-0 position-relative">
+          <div id="clouds" className="">
+            <div className="cloud x1">1</div>
+            <div className="cloud x2">2</div>
+            <div className="cloud x3">3</div>
+            <div className="cloud x4">4</div>
+            <div className="cloud x5">5</div>
+            <div className="cloud x6">6</div>
+          </div>
+        </div>
         <div className="container">
           <div className="navbar d-flex flex-column-reverse flex-sm-row justify-content-between mb-1 mb-sm-5 animate__animated  animate__backInDown animate__delay-1s">
             <div className="navbar-name">
               <a href="/#">early-bird.dev</a>
             </div>
-            <div className="navbar-social d-flex gap-2 gap-md-4 pb-1 pb-md-3 ">
+            <div className="navbar-social d-flex align-items-center justify-content-center gap-2 gap-md-4 pb-1 pb-md-3 ">
               <a
                 href="https://github.com/earlybird-dev"
                 rel="noreferrer"
@@ -37,16 +47,6 @@ const Header = () => {
               </a>
             </div>
           </div>
-          <div className="w-100 position-relative">
-            <div id="clouds">
-              <div className="cloud x1"></div>
-              <div className="cloud x2"></div>
-              <div className="cloud x3"></div>
-              <div className="cloud x4"></div>
-              <div className="cloud x5"></div>
-            </div>
-          </div>
-
           <div className="intro d-flex flex-column gap-4">
             <div className="logo d-flex align-items-center justify-content-center animate__animated  animate__lightSpeedInLeft animate__delay-1s">
               <img src={logo} alt="logo" className="" />
@@ -62,8 +62,8 @@ const Header = () => {
             </div>
             <p className="">
               Based in Australia, I'm a front-end developer passionate about
-              building accessible web apps that users love. Happy to relocate to
-              anywhere in the country.
+              building accessible web apps that users love. Willing to relocate
+              to anywhere in the country.
             </p>
             <div className="d-flex flex-column ">
               <a href="#skills" className="">
